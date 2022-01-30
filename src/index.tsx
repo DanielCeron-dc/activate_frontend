8e3aec228c6d6b5eb6dffa2bc729e31ef3df44b8
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2022-01-30 18:28:46 -0500

    React router dom added Co-authored-by: Juan Andres <Juanandress@users.noreply.github.com> Co-authored-by: laurarengifo9 <laurarengifo9@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+import AuthProvider from './Auth/AuthContext/AuthProvider';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
